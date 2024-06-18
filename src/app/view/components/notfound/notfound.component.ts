b45ca004fc236f160dafd3ef3057d9b078788c1c
eduardoMessKL <eduardomk408@gmail.com>
2024-06-17 21:48:28 -0300

    Criação Component NotFound

---
+++ src/app/view/components/notfound/notfound.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css']
+})
+export class NotfoundComponent {
+
+  constructor(
+    private router: Router
+  ){}
+
+  navigateToHome(){
+    this.router.navigate(['/art-catalog'])
+  }
+}

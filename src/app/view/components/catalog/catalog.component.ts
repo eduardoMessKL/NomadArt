34c5a3ad3f38190f4599be0c187553746cd765fd
eduardoMessKL <eduardomk408@gmail.com>
2024-06-27 23:11:13 -0300

    Catalog - front

--- src/app/view/components/catalog/catalog.component.ts
+++ src/app/view/components/catalog/catalog.component.ts
@@ -1,48 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ArtService } from 'src/app/model/service/artService/art.service';
+import { AuthService } from 'src/app/model/service/authService/auth.service';
+
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent implements OnInit {
   arts: any[] = [];
+  artistId: string = '';
+
   filteredArts: any[] = [];
   techniques: string[] = ['Óleo', 'Aquarela', 'Digital', 'Acrílica', 'Outra'];
   selectedTechnique: string = '';
+
   searchText: string = '';
 
+  isLoggedIn: boolean = false;
   techniqueDetails: {[key: string]: {text: string, imageUrl: string}} = {
     'Óleo': {
-      text: 'A pintura a oil é uma técnica artística na qual pigmentos são suspensos em oil secante, formando uma tinta de textura densa e brilho duradouro.',
-      imageUrl: 'path/to/oleo-image.jpg'
+      text: 'A pintura a óleo é uma técnica artística na qual pigmentos são suspensos em óleo secante, formando uma tinta de textura densa e brilho duradouro.',
+      imageUrl: '/assets/oil.jpg'
     },
     'Aquarela': {
       text: 'A aquarela é uma técnica de pintura em que os pigmentos são dissolvidos em água, resultando em uma obra de arte com tons suaves e transparências.',
-      imageUrl: 'path/to/aquarela-image.jpg'
+      imageUrl: '/assets/water.jpg'
     },
     'Digital': {
       text: 'A arte digital é criada em plataformas digitais, utilizando softwares e ferramentas específicas para criar ilustrações, pinturas e designs gráficos.',
-      imageUrl: 'path/to/digital-image.jpg'
+      imageUrl: '/assets/digital.jpg'
     },
     'Acrílica': {
       text: 'A pintura acrílica usa tintas à base de polímeros acrílicos que secam rapidamente, permitindo cores vibrantes e variadas técnicas de aplicação.',
-      imageUrl: 'path/to/acrilico-image.jpg'
+      imageUrl: '/assets/acrylic.jpg'
     },
     'Outra': {
-      text: 'Outra',
-      imageUrl: 'path'
+      text: 'Nesta seção se encontram todas as outras formas de pintura, descubra novas técnicas!',
+      imageUrl: '/assets/other.jpg'
     }
 
   }
 
-
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private artService: ArtService,
+    private authService: AuthService
   ) {
+    this.artistId = this.route.snapshot.paramMap.get('id')!;
   }
 
   ngOnInit() {
@@ -50,6 +58,10 @@ export class CatalogComponent implements OnInit {
       this.arts = arts;
       this.filteredArts = arts;
     });
+
+    this.authService.isLoggedIn().subscribe(loggedIn =>{
+      this.isLoggedIn = loggedIn;
+    })
   }
 
   filterArts(event: Event) {
@@ -77,5 +89,15 @@ export class CatalogComponent implements OnInit {
 
   viewArt(art: any) {
     this.router.navigate([`/description-art/${art.artistId}/${art.id}`]);
+  }
+
+  async navigateToPublish(): Promise<void> {
+    const userId = await this.authService.getCurrentUserId();
+    if (userId) {
+      this.router.navigate([`profile/${userId}/publish-art`]);
+    } else {
+      this.router.navigate([`/signin`]);
+      console.error('User not logged in');
+    }
   }
 }

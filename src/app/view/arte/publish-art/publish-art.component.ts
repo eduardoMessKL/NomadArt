31d58314d456a530f409f24ba29027f48f147bf7
eduardoMessKL <eduardomk408@gmail.com>
2024-07-03 21:16:11 -0300

    Publish-Art - Front-End Inteiro + Atributo Ano que é um Select

--- src/app/view/arte/publish-art/publish-art.component.ts
+++ src/app/view/arte/publish-art/publish-art.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { finalize } from 'rxjs/operators';
 import { ArtService } from 'src/app/model/service/artService/art.service';
+import { AuthService } from 'src/app/model/service/authService/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-publish-art',
@@ -15,28 +17,51 @@ export class PublishArtComponent {
     title: '',
     description: '',
     technique: '',
-    imageUrl: ''
+    year: '',
+    imageUrl: '',
   };
   selectedFile: File | null = null;
+  selectedImageUrl: string | ArrayBuffer | null = null;
   errorMessage: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private artService: ArtService,
-    private storage: AngularFireStorage
+    private authService: AuthService,
+    private storage: AngularFireStorage,
+    private spinner: NgxSpinnerService
   ) {
     this.artistId = this.route.snapshot.paramMap.get('id')!;
   }
 
   onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
+    const file = event.target.files[0];
+    this.selectedFile = file;
+
+    if(file){
+      const reader = new FileReader();
+      reader.onload = (e) =>{
+        const result = e.target?.result;
+        if(result !== undefined){
+          this.selectedImageUrl = result;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
   }
 
   onSubmit() {
     if (!this.art.title || !this.art.description || !this.art.technique || !this.selectedFile) {
       this.errorMessage = 'Preencha todos os campos!';
       return;
+    }
+    if(this.art.title.length > 50){
+      this.errorMessage = 'Título não pode ter mais de 50 caracteres!';
+      return;
+    }
+    if(this.art.description.length > 500){
+      this.errorMessage = 'Descrição não pode ter mais de 500 caracteres!';
     }
 
     const filePath = `art_images/${this.artistId}/${this.selectedFile.name}`;
@@ -53,5 +78,19 @@ export class PublishArtComponent {
         });
       })
     ).subscribe();
+
+    this.spinner.show();
+    setTimeout(()=>{  
+      this.spinner.hide()  
+    }, 5000)
+  }
+
+  async navigateToProfile(): Promise<void> {
+    const userId = await this.authService.getCurrentUserId();
+    if (userId) {
+      this.router.navigate([`profile/${userId}`]);
+    } else {
+      console.error('User not logged in');
+    }
   }
 }

c3841e309e9ba6edc489f964096b963752c753e9
eduardoMessKL <eduardomk408@gmail.com>
2024-07-03 21:19:13 -0300

    authService - Agora o delete está deletando no Authentication e no Storage tambem

--- src/app/model/service/authService/auth.service.ts
+++ src/app/model/service/authService/auth.service.ts
@@ -2,70 +2,84 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, from, map, switchMap } from 'rxjs';
+import { ArtService } from '../artService/art.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject = new BehaviorSubject(null);
 
   constructor(
     private fireAuth: AngularFireAuth, 
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private storage: AngularFireStorage,
+    private artService: ArtService
   ) { }
 
-  // Cadastro de usuário
+  //registro de um artista
   register(email: string, password: string){
     return this.fireAuth.createUserWithEmailAndPassword(email, password);
   }
 
-  // Login de usuário
+  //login 
   login(email: string, password: string){
     return this.fireAuth.signInWithEmailAndPassword(email, password);
   }
 
-  // Logout
+  //logout
   logout(){
     return this.fireAuth.signOut();
   }
 
-  // Salvar informações adicionais do usuário
+  //salva as informações adicionais do artista criando uma coleção chamada artistas
   saveArtistProfile(userId: string, userProfile: any){
     return this.firestore.collection('artistas').doc(userId).set(userProfile);
   }
 
-  // Obter perfil do artista
-  getArtistProfile(userId: string): Observable<any> {
+  //obter o perfil do artista
+  getArtistProfile(userId: string){
     return this.firestore.collection('artistas').doc(userId).valueChanges();
   }
-  
-  // Deletar perfil do artista
-  deleteArtistProfile(userId: string): Promise<void> {
-    return this.firestore.collection('artistas').doc(userId).delete().then(()=>{
-      return this.deleteUser();
+
+  // apagar o perfil e tambem a auth no firebase
+  deleteArtistProfile(userId: string) {
+    return this.artService.deleteAllArtsOfArtist(userId).pipe(
+      switchMap(() => from(this.firestore.collection('artistas').doc(userId).delete())),
+      switchMap(() => from(this.deleteUser())),
+      switchMap(() => this.deleteUserImages(userId))
+    ).toPromise();
+  }
+
+  private deleteUser() {
+    return this.fireAuth.currentUser.then(user => {
+      if (user) {
+        return user.delete();
+      }
+      throw new Error('No user currently authenticated');
     });
   }
 
-  async deleteUser(): Promise<void> {
-    const user = await this.fireAuth.currentUser;
+  private deleteUserImages(userId: string) {
+    const profileImageRef = this.storage.ref(`profile_images/${userId}`);
+    const artImagesRef = this.storage.ref(`art_images/${userId}`);
 
-  if(user){
-    return user.delete().then(()=>{
-
-    })
-  } else{
-    return Promise.reject();
-  } 
+    return from(profileImageRef.delete()).pipe(
+      switchMap(() => artImagesRef.listAll()),
+      switchMap((listResult) => {
+        const deletePromises = listResult.items.map(itemRef => itemRef.delete());
+        return Promise.all(deletePromises);
+      })
+    );
   }
-
   // Atualizar perfil do artista
   updateArtistProfile(userId: string, userProfile: any){
     return this.firestore.collection('artistas').doc(userId).update(userProfile);
   }
 
-  async getCurrentUserId(): Promise<string | null> {
+  async getCurrentUserId(){
     const user = await this.fireAuth.currentUser;
     return user ? user.uid : null;
   }  

fb08703e2abfeaf650019e79688108688990c4b3
eduardoMessKL <eduardomk408@gmail.com>
2024-07-03 21:16:33 -0300

    Manage - Spinner + Front-end Inteiro

--- src/app/view/arte/manage-art/manage-art.component.ts
+++ src/app/view/arte/manage-art/manage-art.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { ArtService } from 'src/app/model/service/artService/art.service';
 
 @Component({
@@ -14,7 +15,8 @@ export class ManageArtComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private artService: ArtService
+    private artService: ArtService,
+    private spinner: NgxSpinnerService
   ) {
     this.artistId = this.route.snapshot.paramMap.get('id')!;
   }
@@ -35,9 +37,20 @@ export class ManageArtComponent implements OnInit {
 
   deleteArt(artId: string) {
     if (confirm('Tem certeza que deseja deletar esta arte?')) {
-      this.artService.deleteArt(this.artistId, artId).then(() => {
-        this.arts = this.arts.filter(art => art.id !== artId);
-      });
+      this.spinner.show();
+      setTimeout(()=>{ 
+        this.spinner.hide()   
+      }, 5000);
+        this.artService.deleteArt(this.artistId, artId).then(() => {
+          this.arts = this.arts.filter(art => art.id !== artId);
+          console.log('Arte EXCLUÍDA com Sucesso')
+        });
+    } else{
+      console.log('Exclusão de arte CANCELADA')
     }
+  }
+
+  navigateToProfile(){
+    this.router.navigate([`/profile/${this.artistId}`])
   }
 }

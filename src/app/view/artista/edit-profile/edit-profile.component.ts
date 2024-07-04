1aab70b42bdf4608736946b4ba58942b2cd9c973
eduardoMessKL <eduardomk408@gmail.com>
2024-07-03 21:15:12 -0300

    Edit - Front-end Inteiro

--- src/app/view/artista/edit-profile/edit-profile.component.ts
+++ src/app/view/artista/edit-profile/edit-profile.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from '../../../model/service/authService/auth.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { finalize } from 'rxjs/operators';
 import { AlertService } from 'src/app/common/alert.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ArtService } from 'src/app/model/service/artService/art.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -15,6 +17,7 @@ export class EditProfileComponent implements OnInit {
     name: '',
     profileDescription: '',
     country: '',
+    instagram: '',
     techniques: {
       oil: false,
       acrylic: false,
@@ -25,60 +28,78 @@ export class EditProfileComponent implements OnInit {
   };
   artistId: string;
   selectedFile: File | null = null;
+  selectedImageUrl: string | ArrayBuffer | null =  null;
   errorMessage: string = '';
-  userId: any;
-  currentUserId: any
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
+    private artService: ArtService,
     private storage: AngularFireStorage,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private spinner: NgxSpinnerService
   ) {
     this.artistId = this.route.snapshot.paramMap.get('id')!;
   }
 
   async ngOnInit() {
-    this.userId = this.route.snapshot.paramMap.get('id');
-    this.currentUserId = await this.authService.getCurrentUserId();  
-
     this.authService.getArtistProfile(this.artistId).subscribe(artist => {
       this.artist = artist;
     });
-    console.log('Usuáio Logado: ', this.authService.getCurrentUserId())
   }
 
   onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
+    const file = event.target.files[0];
+    this.selectedFile = file;
+
+    if(file){
+      const reader = new FileReader();
+      reader.onload = (e) =>{
+        const result = e.target?.result;
+        if(result !== undefined){
+          this.selectedImageUrl = result;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
   }
 
+
   onSubmit() {
-    if (!this.artist.name || !this.artist.profileDescription || !this.artist.country || !this.artist.techniques) {
-      this.errorMessage = 'Preencha todos os campos!';
-      return;
-    }
+    this.spinner.show();
+    setTimeout(()=>{
+      if (!this.artist.name || !this.artist.profileDescription || !this.artist.country || !this.artist.techniques) {
+        this.errorMessage = 'Preencha todos os campos!';
+        return;
+      }
+  
+      if (this.artist.name.length > 50){
+        this.errorMessage = 'Mínimo de caracteres para o nome é de 50 elementos!'; 
+      }
 
-    if (this.artist.profileDescription.length > 200) {
-      this.errorMessage = 'Máximo de caracteres para a descrição é de 200 elementos!';
-      return;
-    }
-
-    if (this.selectedFile) {
-      const filePath = `profile_images/${this.artistId}`;
-      const fileRef = this.storage.ref(filePath);
-      const uploadTask = this.storage.upload(filePath, this.selectedFile);
-
-      uploadTask.snapshotChanges().pipe(
-        finalize(() => {
-          fileRef.getDownloadURL().subscribe(url => {
-            this.saveArtistProfile(url);
-          });
-        })
-      ).subscribe();
-    } else {
-      this.saveArtistProfile(this.artist.profileImageUrl);
-    }
+      if (this.artist.profileDescription.length > 500) {
+        this.errorMessage = 'Máximo de caracteres para a descrição é de 500 elementos!';
+        return;
+      }
+  
+      if (this.selectedFile) {
+        const filePath = `profile_images/${this.artistId}`;
+        const fileRef = this.storage.ref(filePath);
+        const uploadTask = this.storage.upload(filePath, this.selectedFile);
+  
+        uploadTask.snapshotChanges().pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe(url => {
+              this.saveArtistProfile(url);
+            });
+          })
+        ).subscribe();
+      } else {
+        this.saveArtistProfile(this.artist.profileImageUrl);
+      }
+      this.spinner.hide()
+    }, 2000)
   }
 
   saveArtistProfile(profileImageUrl: string | null) {
@@ -86,20 +107,37 @@ export class EditProfileComponent implements OnInit {
       name: this.artist.name,
       profileDescription: this.artist.profileDescription,
       country: this.artist.country,
+      instagram: this.artist.instagram,
       techniques: this.artist.techniques,
       profileImageUrl: profileImageUrl
     };
     this.authService.updateArtistProfile(this.artistId, artistProfile)
       .then(() => {
-        this.alertService.showAlert('Perfil atualizado com sucesso!');
+        this.alertService.showAlert('Perfil ATUALIZADO com sucesso');
         this.router.navigate([`/profile/${this.artistId}`]);
       })
-      .catch(error => {
-        console.error('Error updating artist profile', error);
-      });
   }
 
   cancelar(){
     this.router.navigate([`/profile/${this.artistId}`]);
+    console.log('Edição de perfil CANCELADA')
   }
+
+  deleteProfile(): void {
+    if (confirm('Você tem certeza que quer apagar seu perfil?')) {
+        this.router.navigate(['/signin']);
+      
+        if (this.artistId) {
+          this.authService.deleteArtistProfile(this.artistId)
+            .then(() => {
+              console.log("Perfil e artes deletados com sucesso");
+            })
+            .catch(error => {
+              console.error("Erro ao deletar o perfil: ", error);
+            });
+        } else {
+          alert('ID do artista não encontrado.');
+        }
+    }
+  } 
 }
